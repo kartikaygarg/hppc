1146597feca17d4eb1944fa7e4e1d6c9654ec621
kartikaygarg <garg.kartikay@gmail.com>
2016-08-28 21:25:38 +0000

    Update

--- ex2/brownian.cpp
+++ ex2/brownian.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <thread>
+#include <random>
+#include <time.h>
+#include <chrono>
+using namespace std;
+using ns = chrono::nanoseconds;
+using get_time = chrono::steady_clock;
 
 void main()
 {
-
+	random_device rd;
+	double pos[10000][3];
+	mt19937 mt(rd());
+	std::uniform_real_distribution<double> dist (0,1.0);
+	#pragma omp parallel for schedule(dynamic)
+	{
+		for(i=0;i<10000;++i){
+			
+		}
+	}
 
 }
